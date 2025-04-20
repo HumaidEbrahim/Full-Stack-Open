0f4972b47aba03792e61471b2dd0f61bb91e869f
HumaidEbrahim <32646111+HumaidEbrahim@users.noreply.github.com>
2025-04-20 21:52:12 +0200

    6b

--- ReduxDemo/src/App.jsx
+++ ReduxDemo/src/App.jsx
@@ -1,10 +1,16 @@
+import NewNote from './components/NewNote'
+import Notes from './components/Notes'
+import VisibilityFilter from './components/Visibility'
 
 const App = () => {
 
     return (
       <div>
         <NewNote />
+        <VisibilityFilter />
         <Notes />
       </div>
     )
   }
+
+  export default App
